960b51b5b06f48560200a18e926dd3b42e057449
SeanL94 <sean.little@connect.qut.edu.au>
2016-10-21 14:50:07 +1000

    Dynamically change users progress on columns

--- YWW.core/ViewModels/FirstViewModel.cs
+++ YWW.core/ViewModels/FirstViewModel.cs
@@ -6,6 +6,33 @@ namespace YWW.core.ViewModels
     public class FirstViewModel 
         : MvxViewModel
     {
-       
+        private int _kjIntake = 8000;
+        private int dietPercentage = 0;
+        private string _dietProgress;
+        private string dietPercentageString;
+
+        public string dietProgress
+        {
+            get
+            {
+                if (_kjIntake < 9000)
+                {
+                    dietPercentage = dietPercentage + 25;
+                    dietPercentageString = dietPercentage.ToString();
+                    _dietProgress = "@drawable/flowers" + dietPercentageString;
+                    return _dietProgress;
+                }
+                else
+                {
+                    _dietProgress = "@drawable/flowers0";
+                    return _dietProgress;
+                }
+            }
+            set
+            {
+                SetProperty(ref _dietProgress, value);
+                RaisePropertyChanged(() => dietProgress);
+            }
+        }
     }
 }
